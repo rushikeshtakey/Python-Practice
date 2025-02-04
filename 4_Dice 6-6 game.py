e8ad5e17b28f789ae64867bdb7bde774fbc6fbc1
rushikeshtakey <rushikeshtakey@gmail.com>
2025-02-04 16:28:42 +0530

    improved output of (4th) Dice 6-6 game

--- 4_Dice 6-6 game.py
+++ 4_Dice 6-6 game.py
@@ -3,17 +3,17 @@ from random import *
 p1point=0
 p2point=0
 while True :
-    print("player 1 press d:")
     pl1=input()
-    if (pl1=='d'):
+    print("player 1 press Enter to roll the dice:",pl1,end=" ")
+    if (pl1==''):
         p1 = randint(1,6)
         print(p1)
         if (p1==6):
             p1point+=1
             pl1==0
-            print("Player 1 second chance press d:")
             pl1=input()
-            if pl1=='d':
+            print("Player 1 second chance press  Enter to roll the dice:",pl1,end=" ")
+            if pl1=='':
                 p1 = randint(1,6)
                 print(p1)
                 if p1==6:
@@ -24,17 +24,17 @@ while True :
                 else:
                     p1point=0
     
-    print("player 2 press d:")
     pl2=input()
-    if(pl2=='d'):
+    print("player 2 press Enter to roll the dice:",pl2,end=' ')
+    if(pl2==''):
         p2 = randint(1,6)
         print(p2)
         if (p2==6):
             p2point+=1
             pl2==0
-            print("Player 2 second chance press d:")
-            pl1=input()
-            if pl1=='d':
+            pl2=input()
+            print("Player 2 second chance press Enter to roll the dice:",pl2,end=' ')
+            if pl2=='':
                 p2 = randint(1,6)
                 print(p2)
                 if p2==6:
